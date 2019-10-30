396c38469926724a36b2c5afe83b15a48fda1656
Robby Gamble <robby.gamble2017@gmail.com>
2019-10-30 10:45:53 -0400

    I created a message variable, and added a code to ask my user for input.

--- MIPSProgrammingProject1.s
+++ MIPSProgrammingProject1.s
@@ -1,3 +1,10 @@
 .data
 sum: .word  0
+message1: .asciiz "Input exactly 10 characters."
 
+main:
+
+#I'm about to ask the user to input 10 characters
+li $v0, 4   #Command to print a string
+la $a0, message1    #Loading the string into the argument to enable printing
+syscall #executing command
