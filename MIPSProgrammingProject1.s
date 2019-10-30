72252cf7afe8dec1a3cecb6d08a2893cf5f7094a
Robby Gamble <robby.gamble2017@gmail.com>
2019-10-30 14:00:35 -0400

    I altered the code again to now store the input into a string, and I am beginning to write a loop to analyze my string and add it to my sum.

--- MIPSProgrammingProject1.s
+++ MIPSProgrammingProject1.s
@@ -2,6 +2,7 @@
 sum: .word  0 #I will use this to store the value of the users input
 message1: .asciiz "Input exactly 10 characters." #I'll need this to communicate with the user
 inputarray: .space 50 #I'm creating an array of 50 bytes to store the users characters
+buffer: .space 11
 
 .text
 main:
@@ -13,11 +14,17 @@ syscall #executing command
 
 
 li $v0, 8 #Command to read a string
-la $a0, inputarray #loading space for the array to be stored
+la $a0, buffer #loading space for the array to be stored
+li $a1, 11 #allocating byte space for string to be stored
+move $t0, $a0 #storing string in t0
 syscall #executing command
 
-addi $t0, $gp, inputarray #store the base address of our array into temp register 0
+li $t1, 0 #creating a counter in $t0 and setting the value to 0
+li $t3, 9
 
+loopy:
+
+lb $t2, 0($t0)
 
 
 
