0576d6e1cc73bea4443764f426f49fb45371ae3a
Robby Gamble <robby.gamble2017@gmail.com>
2019-10-30 12:14:16 -0400

    I believe I added an exit code so my program would stop crashing, and I added a line to find the base address of my array.

--- MIPSProgrammingProject1.s
+++ MIPSProgrammingProject1.s
@@ -3,6 +3,7 @@ sum: .word  0 #I will use this to store the value of the users input
 message1: .asciiz "Input exactly 10 characters." #I'll need this to communicate with the user
 inputarray: .space 50 #I'm creating an array of 50 bytes to store the users characters
 
+.text
 main:
 
 #I'm about to ask the user to input 10 characters
@@ -12,6 +13,21 @@ syscall #executing command
 
 
 li $v0, 8 #Command to read a string
+la $a0, inputarray #loading space for the array to be stored
 syscall #executing command
-move inputarray, $v0 #This line stores the character into the array
 
+addi $t0, $gp, inputarray #store the base address of our array into temp register 0
+
+
+
+
+
+
+
+
+
+
+
+
+li $v0, 10 #exit code
+syscall
