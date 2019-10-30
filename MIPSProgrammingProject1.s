968732f944ba7cd2bd1e29d97568303ed076d3b9
Robby Gamble <robby.gamble2017@gmail.com>
2019-10-30 10:59:14 -0400

    I added an input array inside my data section to store my users input. I then added the command to read a string, and I stored that string into my input array

--- MIPSProgrammingProject1.s
+++ MIPSProgrammingProject1.s
@@ -1,6 +1,7 @@
 .data
-sum: .word  0
-message1: .asciiz "Input exactly 10 characters."
+sum: .word  0 #I will use this to store the value of the users input
+message1: .asciiz "Input exactly 10 characters." #I'll need this to communicate with the user
+inputarray: .space 50 #I'm creating an array of 50 bytes to store the users characters
 
 main:
 
@@ -8,3 +9,9 @@ main:
 li $v0, 4   #Command to print a string
 la $a0, message1    #Loading the string into the argument to enable printing
 syscall #executing command
+
+
+li $v0, 8 #Command to read a string
+syscall #executing command
+move inputarray, $v0 #This line stores the character into the array
+
