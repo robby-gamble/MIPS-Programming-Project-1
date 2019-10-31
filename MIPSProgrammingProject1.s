bb318fbe50426e56424f38cdc881f6b7bd79499c
Robby Gamble <robby.gamble2017@gmail.com>
2019-10-31 18:16:22 -0400

    I added a case for characters that are 0-9.

--- MIPSProgrammingProject1.s
+++ MIPSProgrammingProject1.s
@@ -28,14 +28,19 @@ bge $t3, 11, Sum #as long as it is less than 11 it will loop.
 lb $t2, 0($s1) #Loading the value stored in the input at the base address.
 ble $t2, 47, specialCharacters #If the loaded value is less than 47 on the ascii table then it is a special character, so it jumps to the specialCharacter function.
 ble $t2, 57, zeroThroughNine #If the loaded value is less than 58 and greater than 47 on the ascii table then it is a regular number 0-9, so it jumps to the zeroThroughNine function.
-ble $t2, 90, upperCase #If the number is less than 90 and greater than 57 on the ascii table then it is possibly an upperCase number, so it jumps to the upperCase function.
-ble $t2, 122, lowerCase #If the number is less than 122 and greater than 90 it is possibly a lowerCase character, so it jumps to lowerCase function.
+ble $t2, 78, upperCase #If the number is less than 78 and greater than 57 on the ascii table then it is possibly an upperCase number, so it jumps to the upperCase function.
+ble $t2, 110, lowerCase #If the number is less than 110 and greater than 90 it is possibly a lowerCase character, so it jumps to lowerCase function.
 
 
 specialCharacters:
 sub $s2, $t2, $t2 #If an object is a special character it will elimate itself, and then it will store its value into register s2
 add sum, sum, $s2 #The value is then added to the sum.
 
+
+
+zeroThroughNine:
+sub $s2, $t2, 48 #Because the value of character 0 is 48 in decimal to properly calculate my sum I need to subtract 48 from the input value.
+add sum, sum, $s2 #The value is then added to the sum.
 
 
 
