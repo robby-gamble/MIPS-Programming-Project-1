ad480e1b452a3fbcdd1fe88e984c69a5879aad3c
Robby Gamble <robby.gamble2017@gmail.com>
2019-10-31 18:23:01 -0400

    I added a lowercase function, and fixed a typo.

--- MIPSProgrammingProject1.s
+++ MIPSProgrammingProject1.s
@@ -37,13 +37,16 @@ sub $s2, $t2, $t2 #If an object is a special character it will elimate itself, a
 add sum, sum, $s2 #The value is then added to the sum.
 
 
-
 zeroThroughNine:
 sub $s2, $t2, 48 #Because the value of character 0 is 48 in decimal to properly calculate my sum I need to subtract 48 from the input value.
 add sum, sum, $s2 #The value is then added to the sum.
 
 upperCase:
 sub $s2, $t2, 55 #With my base system, the value of n is 23. In order to properly represent that value I had to subtract 55 from the input value.
+add sum, sum, $s2 #The value is then added to the sum.
+
+lowerCase:
+sub $s2, $t2, 87 ##With my base system, the value of n is 23. In order to properly represent that value I had to subtract 87 from the input value.
 add sum, sum, $s2 #The value is then added to the sum.
 
 
